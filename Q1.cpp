0116911dea27634110f58da2a0ebf57c92b6d495
ApocalypseRev <borseshlok1203@gmail.com>
2024-01-05 23:46:41 +0530

    File Copy Pasted

---
+++ Q1.cpp
@@ -0,0 +1,25 @@
+/*addition of two numbers*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,s;
+
+    cout<<"Enter First Digit =";
+    cin>>a;
+
+    cout<<"Enter Second Digit=";
+    cin>>b;
+
+    s=a+b;
+    cout<<"Answer="<<s;
+    return 0;
+}
+ 
+output ;- 
+Enter First Digit =5
+Enter Second Digit=5
+Answer=10
+Process returned 0 (0x0)   execution time : 2.358 s
+Press any key to continue.
